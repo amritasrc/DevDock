b4c1a269a0e33c7fd7ff1e3e9d642f8f58e9c2e2
amritasrc <amritakumari13022006@gmail.com>
2026-09-12 21:26:03 +0530

    setup bookmark manager backend

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+export default app
---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import app from './app.js'
+
+const PORT = 3001;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on http://localhost:3001`)
+});
