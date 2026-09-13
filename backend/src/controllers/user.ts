11bd2ab9f860ba0ff453800bec1804c6d8fc8283
amritasrc <amritakumari13022006@gmail.com>
2026-09-13 19:02:23 +0530

    feat: add user registration

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,41 @@
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt'
 import User from "../models/user.js";
 
-async function handle(params:type) {
-    
+async function handleCreateNewUser(req: Request, res: Response) {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        });
+    }
+
+    const existingUser = await User.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
+
+    if (existingUser) {
+        return res.status(409).json({
+            success: false,
+            message: "Username or email already exists"
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    })
+
+    res.status(201).json({
+        success: true,
+        message: "User created successfully"
+    });
 }
